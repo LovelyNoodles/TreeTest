c18521966e21115ae9de40f64b625c018c0d3d3f
RiceNoodles <yyp_it_no92@sina.com>
2018-03-14 11:33:47 +0800

    init

---
+++ src/main/java/com/tree/binary/model/TreeNode.java
@@ -0,0 +1,12 @@
+package com.tree.binary.model;
+
+public class TreeNode {
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+
+	public TreeNode(int val) {
+		this.val = val;
+	}
+
+}
